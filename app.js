377cd9063bd194b536cb7c94c6c5affa9da1728e
SunnySparks <Josediazt6@outlook.com>
2021-09-18 22:34:52 -0500

    Add elements to localstorage and remove them from it

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("change", filterTodo);
@@ -21,6 +22,8 @@ function addTodo(event){
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+    //ADD TODO TO LOCALSTORAGE
+    saveLocalTodos(todoInput.value);
     //checkmark button
     const completedButton = document.createElement('button');
     completedButton.innerHTML= '<i class="fas fa-check-square"></i>';
@@ -44,6 +47,7 @@ function deleteCheck(e){
         const todo = item.parentElement;
         //Animation
         todo.classList.add('fall');
+        removeLocalTodos(todo);
         todo.addEventListener('transitionend', function(){
             todo.remove();
         })
@@ -54,7 +58,6 @@ function deleteCheck(e){
         const todo = item.parentElement;
         todo.classList.toggle('completed');
     }
-    console.log(item.parentElement);
 }
 
 function filterTodo(e) {
@@ -80,4 +83,67 @@ function filterTodo(e) {
                 break;
         }
     });
+}
+
+function saveLocalTodos(todo){
+    //CHECK--HEY Do I already have things in local storage?
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodos(){
+    //CHECK--HEY Do I already have things in local storage?
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.forEach(function(todo){
+        //todo DIV
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+        //create Li
+        const newTodo = document.createElement('li');
+        newTodo.innerText = todo;
+        newTodo.classList.add('todo-item');
+        todoDiv.appendChild(newTodo);
+
+        //checkmark button
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML= '<i class="fas fa-check-square"></i>';
+        completedButton.classList.add('complete-btn');
+        todoDiv.appendChild(completedButton);
+        //check trash button
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML= '<i class="fas fa-trash-alt"></i>';
+        trashButton.classList.add('trash-btn');
+        todoDiv.appendChild(trashButton);
+        //Append to list
+        todoList.appendChild(todoDiv);
+    });
+
+}
+
+function removeLocalTodos(todo){
+    //CHECK--HEY Do I already have things in local storage?
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
+
 }
